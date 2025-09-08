12d0ee599dd038a24adb010c069cac0955bee2e5
karliharsha <2389020@cognizant.com>
2025-09-08 10:50:43 +0530

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("This Java program runs without any user input.");
+        
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
